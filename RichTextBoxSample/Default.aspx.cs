e146df14f045674aa91008e8d23947f1840457cf
Krishna Rana <info2krishnarana@gmail.com>
2019-06-05 23:46:18 +0530

    RichTextBox implementation

---
+++ RichTextBoxSample/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection("Data Source=KRISHNA-WINDOWS;Initial Catalog=dbDemoProject;Persist Security Info=True;User ID=sa;Password=dearsql");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindGridview();
+        }
+    }
+    protected void BindGridview()
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select RichTextData from RichTextBoxData", con);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        gvdetails.DataSource = ds;
+        gvdetails.DataBind();
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("insert into RichTextBoxData(RichTextData) values(@Richtextbox)", con);
+        cmd.Parameters.AddWithValue("@Richtextbox", FreeTextBox1.Text);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        FreeTextBox1.Text = "";
+        BindGridview();
+    }
+}
---
+++ RichTextBoxSample/Default2.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+public partial class _Default2 : System.Web.UI.Page
+{
+    SampleDataContext sampleDataContext = new SampleDataContext();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindGridview();
+        }
+    }
+    protected void BindGridview()
+    {
+        gvdetails.DataSource = sampleDataContext.RichTextBoxDatas.ToList();
+        gvdetails.DataBind();
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        sampleDataContext.RichTextBoxDatas.Add(new RichTextBoxData() {
+            RichTextData = FreeTextBox1.Text.Trim()
+        });
+        sampleDataContext.SaveChanges();
+        BindGridview();
+    }
+}
